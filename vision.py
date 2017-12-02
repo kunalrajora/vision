966b9233e66bd57c134aa6b18fde88dc8c1a5844
Kunal Chaudhary Rajora <crank.kunal@gmail.com>
2017-12-03 00:40:04 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -7,36 +7,15 @@ import math
 import os
 import imutils
 
-# Gaussian smoothing
-kernel_size = 3
-
-# Canny Edge Detector
-low_threshold = 10
-high_threshold = 200
-
 # Region-of-interest vertices
 # We want a trapezoid shape, with bottom edge at the bottom of the image
 trap_bottom_width = 1.2  # width of bottom edge of trapezoid, expressed as percentage of image width
 trap_top_width = 0.07  # ditto for top edge of trapezoid
 trap_height = 0.4  # height of the trapezoid expressed as percentage of image height
 
-# Hough Transform
-rho = 2 # distance resolution in pixels of the Hough grid
-theta = 1 * np.pi/180 # angular resolution in radians of the Hough grid
-threshold = 15	 # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 10 #minimum number of pixels making up a line
-max_line_gap = 20	# maximum gap in pixels between connectable line segments
-
-
 # Helper functions
 
 def region_of_interest(img, vertices):
-	"""
-	Applies an image mask.
-	
-	Only keeps the region of the image defined by the polygon
-	formed from `vertices`. The rest of the image is set to black.
-	"""
 	#defining a blank mask to start with
 	mask = np.zeros_like(img)   
 	
@@ -55,22 +34,6 @@ def region_of_interest(img, vertices):
 	return masked_image
 
 def draw_lines(img, lines, color=[0, 144, 255], thickness=5):
-	"""
-	NOTE: this is the function you might want to use as a starting point once you want to 
-	average/extrapolate the line segments you detect to map out the full
-	extent of the lane (going from the result shown in raw-lines-example.mp4
-	to that shown in P1_example.mp4).  
-	
-	Think about things like separating line segments by their 
-	slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-	line vs. the right line.  Then, you can average the position of each of 
-	the lines and extrapolate to the top and bottom of the lane.
-	
-	This function draws `lines` with `color` and `thickness`.	
-	Lines are drawn on the image inplace (mutates the image).
-	If you want to make the lines semi-transparent, think about combining
-	this function with the weighted_img() function below
-	"""
 	# In case of error, don't draw the line(s)
 	if lines is None:
 		return
@@ -100,8 +63,7 @@ def draw_lines(img, lines, color=[0, 144, 255], thickness=5):
 		
 	lines = new_lines
 	
-	# Split lines into right_lines and left_lines, representing the right and left lane lines
-	# Right/left lane lines must have positive/negative slope, and be on the right/left half of the image
+	# Split lines into right_lines and left_lines
 	right_lines = []
 	left_lines = []
 	for i, line in enumerate(lines):
@@ -151,7 +113,7 @@ def draw_lines(img, lines, color=[0, 144, 255], thickness=5):
 		left_m, left_b = 1, 1
 		draw_left = False
 	
-	# Find 2 end points for right and left lines, used for drawing the line
+	# Find 2 end points for right and left lines, used for drawing the line as by formula
 	# y = m*x + b --> x = (y - b)/m
 	y1 = img.shape[0] 
 	y2 = img.shape[0] * (1 - trap_height)
@@ -232,7 +194,12 @@ def draw_lines(img, lines, color=[0, 144, 255], thickness=5):
 			right_x1-= int((right_x1 - m_lower)/3) 
 			
 		
-	
+# Hough Transform
+rho = 2 # distance resolution in pixels of the Hough grid
+theta = 1 * np.pi/180 # angular resolution in radians of the Hough grid
+threshold = 15	 # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 10
+max_line_gap = 20
 	
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
 	lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
@@ -240,10 +207,15 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
 	draw_lines(line_img, lines)
 	return line_img
 
-# Python 3 has support for cool math symbols.
-
 def weighted_img(img, initial_img, al=0.8, bt=1., ga=0.):
 	return cv2.addWeighted(initial_img, al, img, bt, ga)
+	
+# Gaussian smoothing
+kernel_size = 3
+
+# Canny Edge Detector
+low_threshold = 10
+high_threshold = 200
 	
 	
 def black(image):
@@ -303,7 +275,6 @@ def annotate_image_array(image_in):
 	
 	# Run Hough on edge detected image
 	line_image = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
-	cv2.imwrite("erg.jpg",line_image)
 	# Draw lane lines on the original image
 	initial_image = image_in.astype('uint8')
 	annotated_image = weighted_img(line_image, initial_image)
